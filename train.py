134eb893b0fd29396a0102facccc64d8e711b45f
zhaojiawei10 <107256000+zhaojiawei10@users.noreply.github.com>
2025-10-09 14:15:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ parser.add_argument('--num_classes', type=int,
 parser.add_argument('--max_iterations', type=int,
                     default=30000, help='maximum epoch number to train')
 parser.add_argument('--max_epochs', type=int,
-                    default=6, help='maximum epoch number to train')
+                    default=200, help='maximum epoch number to train')
 parser.add_argument('--batch_size', type=int,
                     default=8, help='batch_size per gpu')
 parser.add_argument('--n_gpu', type=int, default=1, help='total gpu')
